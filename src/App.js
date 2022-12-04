7ced27886efa79102354c69c56bd7f4dbeef2aed
RyoLinEth <115244252+RyoLinEth@users.noreply.github.com>
2022-12-04 20:05:35 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import image from './assets/BNB_4.png'
+import appImage from './assets/AppPic.png'
+import black from './assets/black.png'
+import bgimage from './assets/bg.jpg'
+import bnbgif from './assets/bnb.gif'
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers'
 import contractABI from './contractabi.json'
@@ -21,7 +26,7 @@ function App() {
   const defaultIDOAddress = "0x60e21c1C75E60a966734B4Dd0FE1D3ac7484F00A";
   const [errorMessage, setErrorMessage] = useState(null);
   const [defaultAccount, setDefaultAccount] = useState(null)
-  const [connectButtonText, setConnectButtonText] = useState('Connect Wallet')
+  const [connectButtonText, setConnectButtonText] = useState('连接钱包')
   const [provider, setProvider] = useState(null);
   const [signer, setSigner] = useState(null);
   const [contract, setContract] = useState(null);
@@ -31,7 +36,7 @@ function App() {
   const [copied, setCopied] = useState(false);
   const [correctNetwork, setCorrectNetwork] = useState(null);
 
-  const [hasJoin, setHasJoin] = useState("未參與");
+  const [hasJoin, setHasJoin] = useState("未参与");
   const [directReward, setDirectReward] = useState(0);
   const [indirectReward, setIndirectReward] = useState(0)
   const [whiteReward, setWhiteReward] = useState(0)
@@ -40,9 +45,21 @@ function App() {
   const [web3Number, setWeb3Number] = useState(0)
   const [web3NumberNow, setWeb3NumberNow] = useState(0)
 
-  // useEffect(() => {
-  //   connectWalletHandler()
-  // }, [defaultAccount])
+  async function changingAccount() {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', () => {
+        connectWalletHandler()
+      })
+    }
+  }
+
+  useEffect(() => {
+    changingAccount()
+    setTimeout(
+      async function () {
+        await getReward()
+      }, 1000)
+  }, [defaultAccount])
 
   /*
     ==========================================
@@ -56,9 +73,9 @@ function App() {
   const connectWalletHandler = async () => {
     if (window.ethereum) {
       window.ethereum.request({ method: 'eth_requestAccounts' })
-        .then(result => {
-          accountChangeHandler(result[0]);
-          setConnectButtonText('Wallet Connected');
+        .then(async (result) => {
+          await accountChangeHandler(result[0]);
+          setConnectButtonText(`${result[0].slice(0, 4)}...${result[0].slice(-4)}`);
         })
     } else {
       setErrorMessage('Need to install MetaMask!')
@@ -69,6 +86,7 @@ function App() {
   const accountChangeHandler = async (newAccount) => {
     checkCorrectNetwork();
     setDefaultAccount(newAccount);
+    window.sessionStorage.setItem('account', newAccount)
     let tempReflink = `https://17demo.pages.dev/invitedBy=${newAccount}`
     // let tempReflink = `localhost:3001/invitedBy=${newAccount}`
     setRefLink(tempReflink);
@@ -96,7 +114,7 @@ function App() {
 
     if (chainId !== netWorkID) {
       setCorrectNetwork(false)
-      alert("Please connect to BSC Testnet")
+      alert("请连接到正确网络")
     } else {
       setCorrectNetwork(true)
     }
@@ -115,7 +133,7 @@ function App() {
   const makePayment = async () => {
 
     if (correctNetwork === false) {
-      alert("You cannot make payment before you change to the OKC Mainnet")
+      alert("在连到正确的网络前 无法参与")
       return;
     }
 
@@ -132,26 +150,36 @@ function App() {
 
   const getReward = async () => {
     let tempDirectReward = await contract.sonPromotionAmount(defaultAccount) * 0.3
+    tempDirectReward = Math.floor(tempDirectReward * 10) / 10
     setDirectReward(tempDirectReward);
+
     let tempIndirectReward = await contract.grandsonPromotionAmount(defaultAccount) * 0.2
+    tempIndirectReward = Math.floor(tempIndirectReward * 10) / 10
     setIndirectReward(tempIndirectReward);
+
     let tempWhiteReward = await contract.whiteTimes(defaultAccount) * 0.3
+    tempWhiteReward = Math.floor(tempWhiteReward * 10) / 10
     setWhiteReward(tempWhiteReward);
+
     let tempAutoInvestReward = await contract.autoInvestTimes(defaultAccount) * 0.95
+    tempAutoInvestReward = Math.floor(tempAutoInvestReward * 100) / 100
     setAutoInvestReward(tempAutoInvestReward);
 
     let web3Number = await contract.viewJoinIndexForCertainAddress(defaultAccount)
+    if (web3Number.length === 0) setWeb3Number("0");
+
     if (web3Number.length > 0) {
       const initArray = [defaultAccount];
       initArray.pop();
       let index;
       for (index = 0; index < web3Number.length; index++) {
-        if (index != web3Number.length - 1)
+        if (index !== web3Number.length - 1)
           initArray.push(`${parseInt(web3Number[index]._hex)},`)
         else
           initArray.push(parseInt(web3Number[index]._hex))
       }
       console.log(initArray);
+
       setWeb3Number(initArray);
     }
     let totalNumber = await contract.IDOTimes()
@@ -160,6 +188,7 @@ function App() {
 
     let tempHasJoin = await contract.hasJoined(defaultAccount)
     if (tempHasJoin === true) setHasJoin("已加入")
+    if (tempHasJoin === false) setHasJoin("未参与")
   }
 
   const seeFather = async () => {
@@ -182,80 +211,103 @@ function App() {
 
   const alertCopied = () => {
     if (defaultAccount === null) {
-      alert("尚未連接錢包，無複製連結")
+      alert("尚未连接钱包，无法复制连结")
+      return;
     }
-    alert(`邀請連結已生成: ${refLink}`)
+    alert(`邀请连结已生成: ${refLink}`)
+  }
+  var bgstyle = {
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${bgimage})`
   }
 
+  var divBG = {
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${black})`
+  }
+  var divGIF = {
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${bnbgif})`,
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Demo Only</h2>
-        <button onClick={connectWalletHandler}>{connectButtonText}</button>
-        <h5> Address : {defaultAccount} </h5>
-        {errorMessage}
+    <div className="App" style={bgstyle}>
 
-        <CopyToClipboard text={refLink} onCopy={() => setCopied(true)}>
-          <button id="inviteLink" className="tf-button btn-effect" onClick={alertCopied}>
-            Copy Invite Link
-          </button>
-        </CopyToClipboard>
+      <div className="App-header">
+        <img src={appImage} id="TitleImage" />
+        <h2 id="Title">STARLINK</h2>
+        <button onClick={connectWalletHandler} id="ConnectButton" >{connectButtonText}</button>
+      </div>
+      <div className="App-body">
+        <div className="ButtonDiv" style={divBG}>
+          <span>
+            <h5 align="left">点击下方图片 参与 Starlink Web3.0 布局</h5>
+            <img src={image} id="PaymentImage" onClick={makePayment} />
+          </span>
+          <br />
+          <span>
+            <h5>点击下方按钮 复制专属邀请连结</h5>
+            <CopyToClipboard text={refLink} onCopy={() => setCopied(true)}>
+              <button id="copyLinkBtn" className="tf-button btn-effect" onClick={alertCopied}>
+                复制邀请连结
+              </button>
+            </CopyToClipboard>
+          </span>
+        </div>
 
-        <br />
-        <button onClick={makePayment} >Make Payment</button>
-        <br />
-        <button onClick={getReward} >查看我的收益</button>
-        <table className="StatusTable">
-          <thead><tr><td className="headTop">個人資料</td></tr></thead>
+        <table className="StatusTable" style={divBG}>
+          <thead><tr><td className="TableTop">个人资料</td></tr></thead>
           <tbody>
             <tr>
-              <td align="left">身分組 :</td>
+              <td align="left">身分组 :</td>
               <td align="right">{hasJoin}</td>
             </tr>
             <tr>
-              <td align="left">身分編號  :</td>
+              <td align="left">身分编号  :</td>
               <td align="right">{web3Number}</td>
             </tr>
           </tbody>
         </table>
-        <table className="RewardTable">
-          <thead><tr><td className="headTop">個人獎勵</td></tr></thead>
+        <table className="RewardTable" style={divBG}>
+          <thead><tr><td className="TableTop">个人奖励</td></tr></thead>
           <tbody>
             <tr>
-              <td align="left">直推獎勵  :</td>
+              <td align="left">直推奖励  :</td>
               <td align="right">{directReward}  BNB</td>
             </tr>
             <tr>
-              <td align="left">間推獎勵  :</td>
+              <td align="left">间推奖励  :</td>
               <td align="right">{indirectReward}  BNB</td>
             </tr>
             <tr>
-              <td align="left">共識推廣獎勵  :</td>
+              <td align="left">共识推广奖励  :</td>
               <td align="right">{whiteReward}  BNB</td>
             </tr>
             <tr>
-              <td align="left">Web3.0幸運獎勵  :</td>
+              <td align="left">Web3.0幸运奖励  :</td>
               <td align="right">{autoInvestReward}  BNB</td>
             </tr>
           </tbody>
         </table>
-        <table className="Web3Table">
-          <thead><tr><td className="headTop">Web3.0 幸運獎池</td></tr></thead>
+        <table className="Web3Table" style={divGIF}>
+          <thead><tr><td className="TableTop">Web3.0 幸运奖池</td></tr></thead>
           <tbody>
             <tr>
-              <td align="left">您的幸運號碼  :</td>
+              <td align="left">您的幸运号码  :</td>
               <td align="right">{web3Number}</td>
             </tr>
             <tr>
-              <td align="left">當前幸運號碼  :</td>
+              <td align="left">当前幸运号码  :</td>
               <td align="right">{web3NumberNow}</td>
             </tr>
           </tbody>
         </table>
-        <button onClick={seeFather} > See Father</button>
+        {/* <button onClick={seeFather} > See Father</button>
         <h5>你爹  ：{father}</h5>
-        <h5>你祖先：{ancestor}</h5>
-      </header>
+        <h5>你祖先：{ancestor}</h5> */}
+      </div>
     </div>
   );
 }
